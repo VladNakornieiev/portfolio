248d09f35f3cccc528d40a61c1c381c6008b6259
Dark_Matter <66900494+VladNakornieiev@users.noreply.github.com>
2024-02-28 07:27:18 +0200

    Update hygiene.js

--- hack-your-brain/js/hygiene.js
+++ hack-your-brain/js/hygiene.js
@@ -21,6 +21,8 @@ next.addEventListener('click', function () {
     changeDate(1);
 });
 
+updateCheckboxState();
+
 function changeDate(days) {
     let newDate = new Date(year, month - 1, day + days);
     year = newDate.getFullYear();
@@ -28,8 +30,46 @@ function changeDate(days) {
     day = newDate.getDate();
 
     title.textContent = `${day}/${month}/${year}`;
+
+    getInfoCheck(`${day}/${month}/${year}`);
+    updateCheckboxState();
 }
 
-previous.addEventListener('click', function() {
-    
-})
+function getInfoCheck(date) {
+    checkboxes.forEach(function (checkbox, index) {
+        checkbox.addEventListener('click', function () {
+            updateHygieneCalendar(date, index, checkbox.checked);
+        });
+    });
+}
+
+function updateHygieneCalendar(date, checkboxIndex, isChecked) {
+    // Перевірка, чи вже існує об'єкт для даної дати
+    let existingEntry = hygieneCalendar.find(entry => entry.date === date);
+
+    if (existingEntry) {
+        // Оновлення існуючого об'єкта
+        existingEntry[checkboxIndex] = isChecked;
+    } else {
+        // Створення нового об'єкта
+        let newEntry = {
+            date: date,
+            [checkboxIndex]: isChecked
+        };
+        hygieneCalendar.push(newEntry);
+    }
+
+    // Зберігаємо оновлений масив у локальному сховищі
+    localStorage.setItem('hygieneCalendar', JSON.stringify(hygieneCalendar));
+    console.log(hygieneCalendar);
+}
+
+function updateCheckboxState() {
+    hygieneCalendar.forEach(function (entry) {
+        checkboxes.forEach(function (checkbox, index) {
+            if (entry[index] !== undefined) {
+                checkbox.checked = entry[index];
+            }
+        });
+    });
+}
